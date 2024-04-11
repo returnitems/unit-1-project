28c5392c00e1da5186f1ec260d484aa145198aba
diwanumang <diwan.umang@gmail.com>
2024-04-11 10:47:03 -0400

    Added a cookie counting feature and prints cookie count

--- app.js
+++ app.js
@@ -2,14 +2,28 @@ const button = document.querySelector("#button");
 const buzzer = document.querySelector("#buzzer");
 const resetButton = document.querySelector("#reset");
 const winCookie = document.createElement("p");
+const moreCookies = document.createElement("p");
+
+let cookieCount = 0;
 
 const reset = () => {
     winCookie.textContent = "";
+    cookieCount = 0;
+    moreCookies.style.display = "none";
 }
 
 const showCookie = () => {
     winCookie.textContent = "You won a cookie!!";
     document.querySelector("body").appendChild(winCookie);
+    winCookie.style.display = "flex";
+    
+    cookieCount += 1;
+
+    if (cookieCount > 1) {
+        moreCookies.textContent = "You now have " + cookieCount + " cookies!";
+        document.querySelector("body").appendChild(moreCookies);
+        moreCookies.style.display = "flex";
+    }
 }
 
 resetButton.addEventListener("click", reset);
